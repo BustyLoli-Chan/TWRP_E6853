012833503fd55897fd5af23f98aecb1a4fc92306
Ryan Fenton-Garcia <rjfenton@mtu.edu>
2017-01-01 01:27:49 -0700

    Try to import a bunch config options

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,37 +18,57 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := kryo
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 
 # Kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/Image.gz-dtb
+
+# Boot image
+BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 boot_cpus=0-5
+BOARD_KERNEL_CMDLINE += androidboot.hardware=satsuki
+BOARD_KERNEL_CMDLINE += user_debug=31 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y
+BOARD_KERNEL_CMDLINE += coherent_pool=8M
+BOARD_KERNEL_CMDLINE += sched_enable_power_aware=1
+
+
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+
 #TARGET_KERNEL_SOURCE := kernel/sony/msm8994
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := twrp_defconfig
 TARGET_KERNEL_DEVICE_DEFCONFIG := aosp_kitakami_satsuki
 
-TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/Image.gz-dtb
-
-# Boot image
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff enforcing=0
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-
 # Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 5513412608
+#Reserve space for data encryption (24360517632-16384)
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 24360501248
+BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+
+PLATFORM_COMMON_PATH := device/sony/kitakami
+TARGET_RECOVERY_FSTAB = $(PLATFORM_COMMON_PATH)/rootdir/fstab.kitakami
+
+# SELinux
+BOARD_SEPOLICY_DIRS += $(PLATFORM_COMMON_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += device/sony/sepolicy
+
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x004000000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x004000000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x180000000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0xD1A533000
-BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x032000000
-BOARD_FLASH_BLOCK_SIZE := 0x40000
 
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
